861f111c842eaee9e2d3ed613a565a34b5db0d8c
Floy Collis <floy@email.unc.edu>
2021-08-30 11:20:25 -0400

    ex01 coding changes

---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,10 @@
+"""This is my program for relational operators in Python"""
+__author__= "730381998" 
+
+A: int = int(input("Left-hand side: "))
+B: int = int(input("Right-hand side: "))
+
+print (str(A) + " < " + str(B) + " is " + "False")
+print (str(A) + " >= " + str (B) + " is " + "True")
+print (str(A) + " == " + str(B) + " is " + "True")
+print (str(A) + " != " + str(B) + " is " + "False")
