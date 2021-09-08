20e2be99a4c56e64c2849c028d9d1b52415a199f
Floy Collis <floy@email.unc.edu>
2021-09-07 23:01:52 -0400

    repeat beat

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -5,13 +5,16 @@ __author__ = "730381998"
 
 # Begin your solution here...
 i : int = 0
-counter : int = 0
+
 beat: str = input("What beat do you want to repeat? ")
 maximum : int = int(input("How many times do you want to repeat it? "))
-counter = 0
-while counter < maximum:  
-    print(beat, end = '')
-    counter = counter + 1
 
-if maximum <= 0:
-    print("No beat...")
+while i < 1: 
+    if maximum > 0: 
+        print((beat + " ") * (maximum))
+    else:
+        print("No beat...")
+    i = i + 1
+
+
+
